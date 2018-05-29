1eab9aca6b1901d10a834a580da2f6570fc0813a
kengo.hayashi <kengo.hayashi@rakuten.com>
2018-05-29 00:44:44 +0000

    tasklist 1

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+
+   <h1>メッセージ一覧</h1>
+   
+   @if (count($tasks) > 0)
+       <ul>
+           @foreach ($tasks as $task)
+              <li>{{ $task->content }}</li>
+           @endforeach
+       </ul>
+       
+       {!! link_to_route('tasks.create' , '新規メッセージの投稿') !!}
+       
+   @endif
+
+<!-- Write content for each page here -->
+
+@endsection
---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+   <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
+   
+   <p>{{ $task->content }}</p>
+
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id]) !!}
+
+<!-- Write content for each page here -->
+
+@endsection
